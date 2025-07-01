9ad7f5b6ad776ffad2fa44357a1ffd472884e169
Vincent Leclerc <vleclerc@inow.fr>
2025-07-01 17:05:12 +0200

    validation

--- src/core/components/forms/InputMail.jsx
+++ src/core/components/forms/InputMail.jsx
@@ -1,15 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Input } from '/src/core/kendo'
 import PropTypes from 'prop-types'
 
-function InputMail({label, ...otherProps}) {
+const regEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+function InputMail({label='Email', validating=false, ...otherProps}) {
+    const [value, setValue] = useState('');
+
+    const onChangeInput = (event) => {
+        setValue(event.target.value);
+    };
+
+    const showError = validating && value && !regEx.test(value);
+
   return (
-    <Input label={label} type="email" {...otherProps} />
-  )
+    <div>
+        <Input label={label} 
+        type="email" {...otherProps}
+        onChange={onChangeInput} 
+        value={value}
+        />
+        {showError && (<div style={{ color: 'red' }}>Adresse mail invalide</div>)}
+     </div>
+    )
 }
 
 InputMail.propTypes = {
   label: PropTypes.string.isRequired,
 }
+/*
+InputMail.defaultProps = {
+  label: 'Email',
+  validating: false,
+}jusqu'à version 17, en v18 directement dans la déclaration de la fonction*/
 
 export default InputMail
--- src/pages/authentication/Login.jsx
+++ src/pages/authentication/Login.jsx
@@ -3,12 +3,14 @@ import React from 'react'
 import InputMail from '../../core/components/forms/InputMail'
 import { Input } from '@progress/kendo-react-inputs'
 
+
+
 function Login() {
   return (
     <div>
       <h1>Connexion</h1>
       <form noValidate>
-        <InputMail label={"Login"} disabled={true} />
+        <InputMail  disabled={false} validating={true} />
       </form>
 
     </div>
