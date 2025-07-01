637149b3fe2b939f31f0f711a8cca40e03e3a5bc
Vincent Leclerc <vleclerc@inow.fr>
2025-07-01 15:45:41 +0200

    state & useeffect

--- src/core/kendo/index.js
+++ src/core/kendo/index.js
@@ -1,4 +1,5 @@
 import { Menu } from "@progress/kendo-react-layout";
 import { Button } from '@progress/kendo-react-buttons';
+import {Grid} from '@progress/kendo-react-grid';
 
-export { Menu, Button };
+export { Menu, Button, Grid };
--- src/pages/brands/BrandsList.jsx
+++ src/pages/brands/BrandsList.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import { Grid, Button } from '/src/core/kendo'
+import React, { useEffect, useState } from 'react'
 
 function BrandsList() {
+  const [brands, setBrands] = useState([]);
+
+
+  const loadData = () =>{
+    const data = [
+      { id: 1, name: 'Audi', image: 'audi.jpg' },
+      { id: 2, name: 'Renault', image: 'renault.jpg' },
+      { id: 3, name: 'BMW', image: 'bmw.jpg' },
+    ];
+    setBrands(data);
+    console.log('Données chargées', brands);
+  }
+
+  useEffect(() => {
+    console.log('UseEffect', brands);
+  }, [brands]);
+
   return (
-    <div>BrandsList</div>
+    <>
+      <h2>Les marques</h2>
+
+      <Grid data={brands}>
+        <Grid.Column field="id" title="ID" />
+        <Grid.Column field="name" title="Nom" />
+        <Grid.Column field="image" title="Image" />
+      </Grid>
+      <Button onClick={loadData}>Charger les données</Button>
+    </>
   )
 }
 
